94bf43d524bb4df416c9e24f16033f9c179e8845
Matthew Draper <matthew@trebex.net>
2020-03-06 16:51:11 +1030

    Linkify mentions so they're a bit more obvious

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def user_link(user)
-    link_to(user.name, user)
+    link_to("@#{user.name}", user)
   end
 
   def time(t)
@@ -9,5 +9,31 @@ module ApplicationHelper
     else
       time_ago_in_words(t, include_seconds: true) + " ago"
     end
+  end
+
+  def chirp_body(chirp)
+    known_mentions = chirp.mentions.index_by { |u| u.name.downcase }
+
+    output = "".html_safe
+    mention = /(?<!\w)@(\w+)/
+
+    scanner = StringScanner.new(chirp.content)
+    previous_pos = 0
+    while scanner.scan_until(mention)
+      output << scanner.pre_match[previous_pos..-1]
+
+      if user = known_mentions[scanner[1].downcase]
+        output << user_link(user)
+      else
+        # Not a real mention
+        output << scanner[0]
+      end
+
+      previous_pos = scanner.pos
+    end
+
+    output << scanner.rest
+
+    output
   end
 end
