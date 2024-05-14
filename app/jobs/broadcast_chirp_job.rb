7e6abe3ceb444497a28fe3ade088f60526b19334
Tim Riley <tim.r@buildkite.com>
2024-05-14 17:18:55 +1000

    Fix argument error when making broadcasts (#66)
    This broke with the upgrade to Ruby 3, which has stricter handling of keyword arguments (the Rails API here expects a real hash, not keyword args).

--- app/jobs/broadcast_chirp_job.rb
+++ app/jobs/broadcast_chirp_job.rb
@@ -2,13 +2,14 @@ class BroadcastChirpJob < ApplicationJob
   queue_as :default
 
   def perform(chirp)
-    html_fragment = ChirpsController.render(partial: "chirps/chirp", locals: { chirp: chirp })
+    chirp_html_fragment = ChirpsController.render(partial: "chirps/chirp", locals: { chirp: chirp })
+    chirp_message = { chirp: chirp_html_fragment }
 
-    ActionCable.server.broadcast "firehose", chirp: html_fragment
-    ChirpsChannel.broadcast_to chirp.author, chirp: html_fragment
+    ActionCable.server.broadcast "firehose", chirp_message
+    ChirpsChannel.broadcast_to chirp.author, chirp_message
 
     chirp.interested_users.each do |follower|
-      TimelinesChannel.broadcast_to follower, chirp: html_fragment
+      TimelinesChannel.broadcast_to follower, chirp_message
     end
   end
 end
