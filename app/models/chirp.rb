0593588150cd792a8bc3877ea9cb12790abafc4d
Mitch Smith <mitchpsmith1998@gmail.com>
2025-04-05 15:44:27 +1000

    Remove redundant specification of chirps/chirp partial

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -18,14 +18,14 @@ class Chirp < ApplicationRecord
   end
 
   def broadcast
-    broadcast_prepend_later_to "firehose", target: "firehose", partial: "chirps/chirp", locals: { chirp: self }
+    broadcast_prepend_later_to "firehose", target: "firehose"
 
     # Update the author's page
-    broadcast_prepend_later_to ["chirps", author.id], target: "chirps", partial: "chirps/chirp", locals: { chirp: self }
+    broadcast_prepend_later_to ["chirps", author.id], target: "chirps"
 
     # Update the timelines of all users who are interested in this chirp
     interested_users.each do |follower|
-      broadcast_prepend_later_to ["timeline", follower.id], target: "timeline", partial: "chirps/chirp", locals: { chirp: self }
+      broadcast_prepend_later_to ["timeline", follower.id], target: "timeline"
     end
   end
 
