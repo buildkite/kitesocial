4bae261f1c13e1fbaba7f7fc72bc937a82ea2628
Mitch Smith <37649155+mitchbne@users.noreply.github.com>
2026-06-10 14:15:47 +1000

    Update seeds to Nintendo characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 # This data can then be loaded with the rails db:seed command (or created alongside the database with db:setup), and reseeded with db:reset
 
-harry = User.create_with(name: "HarryPotter", password: "hedwigrules").find_or_create_by!(email: "harry@hogwarts.com")
+mario = User.create_with(name: "Mario", password: "itsame-mario").find_or_create_by!(email: "mario@mushroomkingdom.com")
 
-ron = User.create_with(name: "RonWeasley", password: "quidditch4life").find_or_create_by!(email: "ron@hogwarts.com")
+luigi = User.create_with(name: "Luigi", password: "green-stache").find_or_create_by!(email: "luigi@mushroomkingdom.com")
 
-hermione = User.create_with(name: "HermioneGranger", password: "schoolis4eva").find_or_create_by!(email: "hermione@hogwarts.com")
+peach = User.create_with(name: "Peach", password: "toadstool-princess").find_or_create_by!(email: "peach@mushroomkingdom.com")
 
-voldemort = User.create_with(name: "Voldemort", password: "thedarkmark").find_or_create_by!(email: "voldy@deatheaters.com")
+bowser = User.create_with(name: "Bowser", password: "koopa-king").find_or_create_by!(email: "bowser@koopatroop.com")
 
-bellatrix = User.create_with(name: "BellatrixLestrange", password: "ilovevoldy").find_or_create_by!(email: "bella@deatheaters.com")
+koopa_troopa = User.create_with(name: "KoopaTroopa", password: "shell-shuffle").find_or_create_by!(email: "koopa.troopa@koopatroop.com")
 
-chirp_1 = Chirp.create(author: harry, content: "First Chirp! Hi friends!")
-chirp_2 = Chirp.create(author: ron, content: "Can't wait for the quidditch world cup!!!!!")
-chirp_3 = Chirp.create(author: hermione, content: "I am rather excited for exams, if only @RonWeasley could focus.")
-chirp_4 = Chirp.create(author: ron, content: "I'm focusing on the important things in life, @HermioneGranger.")
-chirp_5 = Chirp.create(author: bellatrix, content: "Off to do some super secret death eater business!")
-chirp_6 = Chirp.create(author: voldemort, content: "Who wants to join my @HarryPotter hunt? 😎")
-chirp_7 = Chirp.create(author: harry, content: "Wait, what did @Voldemort just say?")
+chirp_1 = Chirp.create(author: mario, content: "First Chirp! Hi friends!")
+chirp_2 = Chirp.create(author: luigi, content: "Can't wait for the Mario Kart Grand Prix!!!!!")
+chirp_3 = Chirp.create(author: peach, content: "I am rather excited for royal duties, if only @Luigi could focus.")
+chirp_4 = Chirp.create(author: luigi, content: "I'm focusing on the important things in life, @Peach.")
+chirp_5 = Chirp.create(author: koopa_troopa, content: "Off to do some super secret Koopa Troop business!")
+chirp_6 = Chirp.create(author: bowser, content: "Who wants to join my @Mario hunt? 😎")
+chirp_7 = Chirp.create(author: mario, content: "Wait, what did @Bowser just say?")
 
-harry.followers << ron
-harry.followers << hermione
-harry.followers << voldemort
+mario.followers << luigi
+mario.followers << peach
+mario.followers << bowser
 
-ron.followers << harry
-ron.followers << hermione
+luigi.followers << mario
+luigi.followers << peach
 
-hermione.followers << harry
-hermione.followers << ron
+peach.followers << mario
+peach.followers << luigi
 
-voldemort.followers << bellatrix
+bowser.followers << koopa_troopa
